1bb22ae4c0999a5e35dc632bcd959a95ea970bff
Marco Cotugno <mcotugno10@gmail.com>
2021-02-16 18:59:55 +0100

    Created teh components and implemented the visual part

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { IncomeExpenses } from './components/IncomeExpenses';
+import { Transaction } from './components/Transaction';
+import { AddTransaction } from './components/AddTransaction';
+
+function App() {
+  return (
+    <div className="App">
+     <Header/>
+     <Balance/>
+     <IncomeExpenses/>
+     <Transaction/>
+     <AddTransaction/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Balance.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Balance = () => {
+    return (
+        <div>
+            <div className="container">
+                <h4>Your Balance</h4>
+                <h1>0.00€</h1>
+            </div>
+        </div>
+    )
+}
